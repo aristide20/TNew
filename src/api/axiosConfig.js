a80ed906417f6e7396591aaa2d5bfb66ad5c32a7
aristide20 <aristidembidapro@gmail.com>
2023-02-27 06:35:06 +0100

    correction 2 before deployment

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const instance = axios.create({baseURL: 'http://transaf-api.onrender.com'});
+const instance = axios.create({baseURL: 'https://transaf-api.onrender.com'});
 
 
 export default instance;
